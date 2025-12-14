71b0a51c45aa29b1fe3226c6d99976f008616abe
Tom Thompson <tomthompsonrph2@gmail.com>
2025-12-14 10:56:43 -0700

    Added SpringInitializr project files
    Added SpringInitializr project files

---
+++ src/main/java/guro/springframework/spring5_jokes_app_v2/Spring5JokesAppV2Application.java
@@ -0,0 +1,13 @@
+package guro.springframework.spring5_jokes_app_v2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring5JokesAppV2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Spring5JokesAppV2Application.class, args);
+	}
+
+}
---
+++ src/test/java/guro/springframework/spring5_jokes_app_v2/Spring5JokesAppV2ApplicationTests.java
@@ -0,0 +1,13 @@
+package guro.springframework.spring5_jokes_app_v2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Spring5JokesAppV2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
